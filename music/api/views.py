1a27437bd5f3a9c497fce8934944efef43431a8b
laintime <hzhe111@163.com>
2023-01-06 21:36:59 +0800

    create room view

--- music/api/views.py
+++ music/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -9,3 +11,8 @@ from .models import Room
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+    def post(self, request, format=None):
+        pass
